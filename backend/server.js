27fef934a6f617df520c13eb2e712a3b9ebe65c5
Mitesh Sodhaparmar <169137816+Miteshsodha@users.noreply.github.com>
2025-07-18 22:14:13 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,32 +7,28 @@ import 'dotenv/config'
 import cartRouter from "./routes/cartRoute.js"
 import orderRouter from "./routes/orderRoute.js"
 
-
-
-
-
-//app config
-const app = express()
-const port = 4000
+// app config
+const app = express();
+const port = process.env.PORT || 4000;
 
 // middleware
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 // db connection
 connectDB();
 
 // api endpoints
-app.use("/api/food",foodRouter)
-app.use("/images",express.static('uploads'))
-app.use("/api/user",userRouter)
-app.use("/api/cart",cartRouter)
-app.use("/api/order",orderRouter)
+app.use("/api/food", foodRouter);
+app.use("/images", express.static('uploads'));
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
-app.get("/",(req,res)=>{
-    res.send("API Working")
-})
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
 
-app.listen(port,()=>{
-    console.log(`Server Started on http://localhost:${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server Started on http://localhost:${port}`);
+});
