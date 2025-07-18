5e1cc7f2341de955c7b56339d8de094460f55fa8
Mitesh Sodhaparmar <169137816+Miteshsodha@users.noreply.github.com>
2025-07-19 01:49:13 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = "http://localhost:4000"
+  const url = "https://food-delivery-website-backend-yfsg.onrender.com"
 
   return (
     <div>
